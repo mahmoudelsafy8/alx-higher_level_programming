a3f742f178d118784256d0d2f970822df9c60b52
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2024-01-08 14:37:00 -0800

    10

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        sentence = None
+    if sentence:
+        len_s = len(sentence)
+    else:
+        len_s = 0
+    return (len_s, sentence if not sentence else entence{:1})
