5ed06716b88d5c1253a579731ccb3e346e90e18c
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2024-01-09 12:05:01 -0800

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    i = len(argv) - 1
+    if i < 1:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    for x in range(i):
+        print("{}: {:s}".format(i + 1,argv[x + 1]))
