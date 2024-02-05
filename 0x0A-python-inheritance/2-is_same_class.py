5c87d1ec22642bcb206d14953f4f9f870d12aeeb
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2024-02-05 11:43:43 -0800

    2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Module for is_same_class mathod'''
+"""Module for is_same_class mathod"""
 def is_same_class(obj, a_class):
-    '''Determines if object is exactly instance of a class'''
+    """Determines if object is exactly instance of a class"""
     return type(obj) == a_class
