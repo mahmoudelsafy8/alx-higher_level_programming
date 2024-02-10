419c3f02676a64efff16219e43f4e1b11fad707c
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2024-02-10 07:35:34 -0800

    3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,10 @@ class Rectangle(Base):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """
+        method returns the dictionary representation of a Rectangle
+        """
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,25 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.geight = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """
+        method that Update the class Square to assigns attributes
+        """
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """
+        method that Update the class Square to assigns attributes
+        """
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
